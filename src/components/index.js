900ee1e7fd5ce5e98d455d3fb3029ce64eb2311e
ikpeku <ikpedaniel07@gmail.com>
2022-06-11 20:02:50 -0400

    home page setup

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as  Navbar } from "./navbar/Navbar"
-export { default as  Button } from "./button/Button"
-export { default as  Count } from "./count/Count"
-export { default as  CTA } from "./cta/CTA"
-export { default as  Card } from "./card/Card"
-export { default as  Footer } from "./card/Footer"
--- src/containers/our_services/index.js
+++
@@ -1,6 +0,0 @@
-import service1 from "../../assets/service1.png"
-import service2 from "../../assets/service2.png"
-
-
-
-export {service1, service2}
